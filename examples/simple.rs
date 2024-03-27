2a3e8d54256c4fc21c168ca234d3e49509a53a97
Wenxuan Zhang <wenxuangm@gmail.com>
2024-03-27 23:37:41 +0800

    feat: add simple example
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

---
+++ examples/simple.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use clap::Parser;
+use rlt::{
+    cli::BenchCli,
+    report::IterReport,
+    runner::{BenchSuite, WorkerState},
+    status::Status,
+};
+use tokio::time::{Duration, Instant};
+
+#[derive(Clone)]
+struct FakeBench;
+
+#[async_trait]
+impl BenchSuite for FakeBench {
+    type RunnerState = ();
+    async fn state(&self) -> Result<Self::RunnerState> {
+        Ok(())
+    }
+
+    async fn bench(&mut self, _: &Self::RunnerState, ws: &mut WorkerState) -> Result<IterReport> {
+        let t = Instant::now();
+
+        // simulate some work
+        tokio::time::sleep(Duration::from_micros(ws.global_seq() % 30)).await;
+        let duration = t.elapsed();
+
+        // simulate status code
+        let status = match ws.worker_seq() % 10 {
+            8..=10 => Status::server_error(500),
+            6..=7 => Status::client_error(400),
+            _ => Status::success(200),
+        };
+
+        // simulate items processed in current iteration
+        let items = ws.worker_seq() % 100;
+        Ok(IterReport { duration, status, bytes: items * 1024, items })
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    rlt::cli::run(&BenchCli::parse(), FakeBench).await
+}
