8b23af0ed72c30154fc73add592c5b5bb71fe056
Wenxuan Zhang <wenxuangm@gmail.com>
2024-04-22 21:51:47 +0800

    refactor: use governor for rate limiting
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- src/runner.rs
+++ src/runner.rs
@@ -1,6 +1,8 @@
 //! This module defines traits for stateful and stateless benchmark suites.
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
+use governor::{Quota, RateLimiter};
+use nonzero_ext::{nonzero, NonZero};
 use std::{
     sync::{
         atomic::{AtomicU64, Ordering},
@@ -12,11 +14,14 @@ use tokio::{
     select,
     sync::{mpsc, watch},
     task::JoinSet,
-    time::MissedTickBehavior,
 };
 use tokio_util::sync::CancellationToken;
 
-use crate::{clock::Clock, report::IterReport};
+use crate::{
+    clock::Clock,
+    // rate_limiter::{self, RateLimiter},
+    report::IterReport,
+};
 
 /// Core options for the benchmark runner.
 #[derive(Clone, Debug)]
@@ -148,20 +153,25 @@ where
 
     /// Run the benchmark.
     pub async fn run(self) -> Result<()> {
-        match self.opts.rate {
-            None => self.bench().await,
-            Some(r) => self.bench_with_rate(r).await,
-        }
-    }
-
-    /// Run the benchmark without a rate limit.
-    async fn bench(self) -> Result<()> {
         let concurrency = self.opts.concurrency;
         let iterations = self.opts.iterations;
+
+        let rli = match self.opts.rate {
+            Some(r) => {
+                let quota = Quota::per_second(
+                    NonZero::new(r).ok_or_else(|| anyhow!("rate limit must be greater than 0, got {}", r))?,
+                )
+                .allow_burst(nonzero!(1u32));
+                let clock = &self.opts.clock;
+                Some(Arc::new(RateLimiter::direct_with_clock(quota, clock)))
+            }
+            None => None,
+        };
 
         let mut set: JoinSet<Result<()>> = JoinSet::new();
         for worker in 0..concurrency {
             let mut b = self.clone();
+            let rli = rli.clone();
             set.spawn(async move {
                 let mut state = b.suite.state(worker).await?;
                 let mut info = IterInfo::new(worker);
@@ -175,6 +185,14 @@ where
                             break;
                         }
                     }
+
+                    if let Some(rli) = &rli {
+                        select! {
+                            _ = cancel.cancelled() => break,
+                            _ = rli.until_ready() => (),
+                        }
+                    }
+
                     select! {
                         _ = cancel.cancelled() => break,
                         _ = b.iteration(&mut state, &info) => (),
@@ -194,76 +212,6 @@ where
                 _ = join_all(&mut set) => (),
             }
         };
-
-        join_all(&mut set).await
-    }
-
-    /// Run the benchmark with a rate limit.
-    async fn bench_with_rate(self, rate: u32) -> Result<()> {
-        let concurrency = self.opts.concurrency;
-        let iterations = self.opts.iterations;
-        let clock = self.opts.clock.clone();
-        let duration = self.opts.duration;
-        let (tx, rx) = flume::bounded(self.opts.concurrency as usize);
-
-        let b = self.clone();
-        tokio::spawn(async move {
-            let mut timer = tokio::time::interval(Duration::from_secs(1) / rate);
-            timer.set_missed_tick_behavior(MissedTickBehavior::Burst);
-            let mut iter = 0;
-            loop {
-                timer.tick().await;
-                if b.paused() {
-                    match b.cancel.is_cancelled() {
-                        false => continue,
-                        true => break,
-                    }
-                }
-                if matches!(duration, Some(duration) if clock.elapsed() >= duration) {
-                    break;
-                }
-                if matches!(iterations, Some(iterations) if iter >= iterations) {
-                    break;
-                }
-                if tx.send_async(()).await.is_err() {
-                    // receiver dropped
-                    break;
-                }
-                iter += 1;
-            }
-        });
-
-        let mut set: JoinSet<Result<()>> = JoinSet::new();
-        for worker in 0..concurrency {
-            let mut b = self.clone();
-            let rx = rx.clone();
-            set.spawn(async move {
-                let mut state = b.suite.state(worker).await?;
-                let mut info = IterInfo::new(worker);
-                let cancel = b.cancel.clone();
-
-                b.suite.setup(&mut state, worker).await?;
-                loop {
-                    select! {
-                        _ = cancel.cancelled() => break,
-                        t = rx.recv_async() => match t {
-                            Ok(_) => {
-                                info.runner_seq = b.seq.fetch_add(1, Ordering::Relaxed);
-                                select! {
-                                    _ = cancel.cancelled() => break,
-                                    _ = b.iteration(&mut state, &info) => (),
-                                }
-                                info.worker_seq += 1;
-                            }
-                            Err(_) => break,
-                        }
-                    }
-                }
-                b.suite.teardown(state, info).await?;
-
-                Ok(())
-            });
-        }
 
         join_all(&mut set).await
     }
